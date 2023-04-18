a208b15b2177c4925e871675e50d5465f97c6013
mehdi <dehghani_m@rocketmail.com>
2023-04-18 16:34:58 +0430

    continue make project

--- app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
+++ app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
@@ -1,26 +1,25 @@
 package com.example.mapcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.DragState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
 private const val TAG = "BasicMapActivity"
 
 val singapore = LatLng(1.35, 103.87)
-val singapore1 = LatLng(1.40, 103.77)
-val singapore2 = LatLng(1.45, 103.77)
+val singapore2 = LatLng(1.40, 103.77)
+val singapore3 = LatLng(1.45, 103.77)
 val defaultCameraPosition = CameraPosition.fromLatLngZoom(singapore,11f)
 
 class BasicMapActivity : ComponentActivity() {
@@ -43,6 +42,7 @@ class BasicMapActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
 fun GoogleMapView(
     modifier: Modifier = Modifier,
@@ -51,5 +51,10 @@ fun GoogleMapView(
     current: @Composable () -> Unit = {}
 ){
     val singaporeState = rememberMarkerState(position = singapore)
-
+    val singapore2State = rememberMarkerState(position = singapore2)
+    val singapore3State = rememberMarkerState(position = singapore3)
+    var circleCenter by remember { mutableStateOf(singapore) }
+    if (singaporeState.dragState == DragState.END){
+        circleCenter = singaporeState.position
+    }
 }
