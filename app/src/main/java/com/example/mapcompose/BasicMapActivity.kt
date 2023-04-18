211dfcdf424e306d124e8944a8d2c0a15e7c4973
mehdi <dehghani_m@rocketmail.com>
2023-04-18 16:28:42 +0430

    continue maps

--- app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
+++ app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
@@ -3,8 +3,18 @@ package com.example.mapcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 
 private const val TAG = "BasicMapActivity"
 
@@ -18,7 +28,28 @@ class BasicMapActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            val isMapLoaded by remember { mutableStateOf(false) }
+
+            val cameraPositionState = rememberCameraPositionState{
+                position = defaultCameraPosition
+            }
+
+            Box(Modifier.fillMaxSize()){
+                GoogleMapView(
+
+                )
+            }
         }
     }
+}
+
+@Composable
+fun GoogleMapView(
+    modifier: Modifier = Modifier,
+    cameraPositionState: CameraPositionState = rememberCameraPositionState(),
+    onMapLoad: () -> Unit = {},
+    current: @Composable () -> Unit = {}
+){
+    val singaporeState = rememberMarkerState(position = singapore)
+
 }
