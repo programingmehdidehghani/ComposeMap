cc673e3ce272782f1ad734ef1426c61a175ac9b3
mehdi <dehghani_m@rocketmail.com>
2023-04-19 16:56:46 +0430

    finish locatonTracking

--- app/src/main/java/com/example/mapcompose/LocationTrackingActivity.kt
+++ app/src/main/java/com/example/mapcompose/LocationTrackingActivity.kt
@@ -1,19 +1,117 @@
 package com.example.mapcompose
 
 import android.location.Location
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.LocationSource
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.shareIn
 import kotlin.random.Random
 
 private const val TAG = "LocationTrackActivity"
 private const val zoom = 8f
 
-class LocationTrackingActivity: ComponentActivity() {
+class LocationTrackingActivity : ComponentActivity() {
 
-    private val locationSource =
+    private val locationSource = MyLocationSource()
+    private var counter = 0
+
+    private val locationFlow = callbackFlow {
+        while (true) {
+          ++ counter
+
+          val location = newLocation()
+            Log.d(TAG,"Location $counter: $location")
+            trySend(location)
+
+            kotlinx.coroutines.delay(2_000)
+        }
+    }.shareIn(
+        lifecycleScope,
+        replay = 0,
+        started = SharingStarted.WhileSubscribed()
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContent {
+            var isMapLoaded by remember { mutableStateOf(false) }
+            val cameraPositionState = rememberCameraPositionState {
+                position = defaultCameraPosition
+            }
+
+            val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
+
+            val locationState = locationFlow.collectAsState(initial = newLocation())
+            
+            LaunchedEffect(locationState.value){
+                Log.d(TAG,"Updating blue dot on map...")
+                locationSource.onLocationChanged(locationState.value)
+
+                Log.d(TAG, "Updating camera position...")
+                val cameraPosition = CameraPosition.fromLatLngZoom(LatLng(locationState.value.latitude,locationState.value.longitude), zoom)
+                cameraPositionState.animate(CameraUpdateFactory.newCameraPosition(cameraPosition), 1_000)
+            }
+            LaunchedEffect(cameraPositionState.isMoving){
+                if (cameraPositionState.isMoving){
+                    Log.d(TAG,"Map camera started moving due to ${cameraPositionState.cameraMoveStartedReason.name}")
+                }
+            }
+            
+            Box(Modifier.fillMaxSize()){
+                GoogleMap(
+                    modifier = Modifier.matchParentSize(),
+                    cameraPositionState = cameraPositionState,
+                    onMapLoaded = {
+                        isMapLoaded = true
+                    },
+                    locationSource = locationSource,
+                    properties = mapProperties
+                )
+                if (!isMapLoaded){
+                    AnimatedVisibility(
+                        modifier = Modifier
+                            .matchParentSize(),
+                        visible = !isMapLoaded,
+                        enter = EnterTransition.None,
+                        exit = fadeOut()
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .background(MaterialTheme.colors.background)
+                                .wrapContentSize()
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
-private class MyLocationSource: LocationSource {
+private class MyLocationSource : LocationSource {
 
     private var listener: LocationSource.OnLocationChangedListener? = null
 
@@ -25,13 +123,13 @@ private class MyLocationSource: LocationSource {
         listener = null
     }
 
-    fun onLocationChanged(location: Location){
+    fun onLocationChanged(location: Location) {
         listener?.onLocationChanged(location)
     }
 
 }
 
-private fun newLocation():Location {
+private fun newLocation(): Location {
     val location = Location("MyLocationProvider")
     location.apply {
         latitude = singapore.latitude + Random.nextFloat()
