13269c5c532a1ceba3809dbbf0c57479acedafc6
mehdi <dehghani_m@rocketmail.com>
2023-04-19 12:52:22 +0430

    make funtion zoom map

--- app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
+++ app/src/main/java/com/example/mapcompose/BasicMapActivity.kt
@@ -5,20 +5,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.*
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 private const val TAG = "BasicMapActivity"
 
 val singapore = LatLng(1.35, 103.87)
 val singapore2 = LatLng(1.40, 103.77)
 val singapore3 = LatLng(1.45, 103.77)
-val defaultCameraPosition = CameraPosition.fromLatLngZoom(singapore,11f)
+val defaultCameraPosition = CameraPosition.fromLatLngZoom(singapore, 11f)
 
 class BasicMapActivity : ComponentActivity() {
 
@@ -27,11 +35,11 @@ class BasicMapActivity : ComponentActivity() {
         setContent {
             val isMapLoaded by remember { mutableStateOf(false) }
 
-            val cameraPositionState = rememberCameraPositionState{
+            val cameraPositionState = rememberCameraPositionState {
                 position = defaultCameraPosition
             }
 
-            Box(Modifier.fillMaxSize()){
+            Box(Modifier.fillMaxSize()) {
                 GoogleMapView(
 
                 )
@@ -40,19 +48,20 @@ class BasicMapActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalContracts::class)
 @SuppressLint("RememberReturnType")
 @Composable
 fun GoogleMapView(
     modifier: Modifier = Modifier,
     cameraPositionState: CameraPositionState = rememberCameraPositionState(),
     onMapLoad: () -> Unit = {},
-    current: @Composable () -> Unit = {}
-){
+    content: @Composable () -> Unit = {}
+) {
     val singaporeState = rememberMarkerState(position = singapore)
     val singapore2State = rememberMarkerState(position = singapore2)
     val singapore3State = rememberMarkerState(position = singapore3)
     var circleCenter by remember { mutableStateOf(singapore) }
-    if (singaporeState.dragState == DragState.END){
+    if (singaporeState.dragState == DragState.END) {
         circleCenter = singaporeState.position
     }
     var uiSettings by remember { mutableStateOf(MapUiSettings(compassEnabled = false)) }
@@ -63,7 +72,7 @@ fun GoogleMapView(
     }
     var mapVisible by remember { mutableStateOf(true) }
 
-    if (mapVisible){
+    if (mapVisible) {
         GoogleMap(
             modifier = modifier,
             cameraPositionState = cameraPositionState,
@@ -71,10 +80,72 @@ fun GoogleMapView(
             uiSettings = uiSettings,
             onMapLoaded = onMapLoad,
             onPOIClick = {
-                Log.d(TAG,"POI clicked: ${it.name}")
+                Log.d(TAG, "POI clicked: ${it.name}")
             }
-        ){
-            
+        ) {
+            val markerClick: (Marker) -> Boolean = {
+                Log.d(TAG, "${it.title} was clicked")
+                cameraPositionState.position?.let { projection ->
+                    Log.d(TAG, "The current projection is: $projection")
+                }
+                false
+            }
+            MarkerInfoWindowContent(
+                state = singaporeState,
+                title = "Zoom in has been tapped $ticker times.",
+                onClick = markerClick,
+                draggable = true
+            ) {
+                Text(it.title ?: "Title", color = Color.Red)
+            }
+            MarkerInfoWindowContent(
+                state = singapore2State,
+                title = "Marker with custom info window.\\nZoom in has been tapped $ticker times.",
+                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE),
+                onClick = markerClick
+            ) {
+                Text(it.title ?: "Title", color = Color.Blue)
+            }
+            Marker(
+                state = singapore3State,
+                title = "Marker in Singapore",
+                onClick = markerClick
+            )
+            Circle(
+                center = circleCenter,
+                fillColor = MaterialTheme.colors.secondary,
+                strokeColor = MaterialTheme.colors.secondaryVariant,
+                radius = 1000.0
+            )
+            content()
+        }
+    }
+    Column {
+
+    }
+}
+
+@Composable
+fun MapTypeControls(
+    onMapTypeClick: (MapType) -> Unit
+) {
+    Row(
+        Modifier.fillMaxWidth()
+            .horizontalScroll(state = ScrollState(0)),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        MapType.values().forEach {
+            MapTypeButton(type = it) { onMapTypeClick(it) }
         }
     }
 }
+
+@Composable
+fun MapTypeButton(type: MapType, onClick: () -> Unit) =
+    MapButton(text = type.toString(), onClick = onClick)
+
+@Composable
+fun ZoomControls(
+    
+)
+
