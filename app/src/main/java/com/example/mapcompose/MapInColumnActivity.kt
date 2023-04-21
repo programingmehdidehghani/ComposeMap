4be9780f186fafd08b8f4d01efdf7314cf8a37d9
mehdi <dehghani_m@rocketmail.com>
2023-04-21 11:42:07 +0430

    make funtion map Incolumn

--- app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
+++ app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
@@ -5,14 +5,25 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
@@ -37,9 +48,42 @@ class MapInColumnActivity : ComponentActivity() {
 
 @Composable
 fun MapInColumn(
-
+    modifier: Modifier = Modifier,
+    cameraPositionState: CameraPositionState,
+    columnScrollingEnabled: Boolean,
+    onMapTouched: () -> Unit,
+    onMapLoaded: () -> Unit
 ){
-    
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colors.background
+    ) {
+        var isMapLoaded by remember { mutableStateOf(false) }
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(
+                    rememberScrollState(),
+                    columnScrollingEnabled
+                ),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Spacer(modifier = Modifier.padding(10.dp))
+            for (i in 1..20){
+                Text(
+                    text = "Item $i",
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .testTag("Item $i")
+                )
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+        }
+        
+    }
+
 }
 
 @Composable
