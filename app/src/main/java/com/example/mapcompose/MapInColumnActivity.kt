4f5d35986a9f7ac2d94d550c0cc1fc579f5bf82c
mehdi <dehghani_m@rocketmail.com>
2023-04-21 12:03:18 +0430

    finishmap columnActivity

--- app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
+++ app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
@@ -3,25 +3,38 @@ package com.example.mapcompose
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.Marker
@@ -31,6 +44,7 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.MarkerInfoWindowContent
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
 private const val TAG = "ScrollingMapActivity"
@@ -40,12 +54,37 @@ class MapInColumnActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val cameraPositionState = rememberCameraPositionState {
+                position = defaultCameraPosition
+            }
+            var columnScrollingEnabled by remember { mutableStateOf(true) }
 
+            LaunchedEffect(cameraPositionState.isMoving) {
+                if (!cameraPositionState.isMoving) {
+                    columnScrollingEnabled = true
+                    Log.d(TAG, "Map camera stopped moving - Enabling column scrolling...")
+                }
+            }
+
+            MapInColumn(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState,
+                columnScrollingEnabled = columnScrollingEnabled,
+                onMapTouched = {
+                    columnScrollingEnabled = false
+                    Log.d(
+                        TAG,
+                        "User touched map - Disabling column scrolling after user touched this Box..."
+                    )
+                },
+                onMapLoaded = {}
+            )
         }
     }
 }
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MapInColumn(
     modifier: Modifier = Modifier,
@@ -53,7 +92,7 @@ fun MapInColumn(
     columnScrollingEnabled: Boolean,
     onMapTouched: () -> Unit,
     onMapLoaded: () -> Unit
-){
+) {
     Surface(
         modifier = modifier,
         color = MaterialTheme.colors.background
@@ -70,7 +109,66 @@ fun MapInColumn(
             horizontalAlignment = Alignment.Start
         ) {
             Spacer(modifier = Modifier.padding(10.dp))
-            for (i in 1..20){
+            for (i in 1..20) {
+                Text(
+                    text = "Item $i",
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .testTag("Item $i")
+                )
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            ) {
+                GoogleMapViewInColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .testTag("Map")
+                        .pointerInteropFilter(
+                            onTouchEvent = {
+                                when (it.action) {
+                                    MotionEvent.ACTION_DOWN -> {
+                                        onMapTouched()
+                                        false
+                                    }
+
+                                    else -> {
+                                        Log.d(
+                                            TAG, "MotionEvent ${it.action} -  this never triggers."
+                                        )
+                                        true
+                                    }
+                                }
+                            }
+                        ),
+                    cameraPositionState = cameraPositionState,
+                    onMapLoaded = {
+                        isMapLoaded = true
+                        onMapLoaded()
+                    },
+                )
+                if (!isMapLoaded) {
+                    androidx.compose.animation.AnimatedVisibility(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        visible = !isMapLoaded,
+                        enter = EnterTransition.None,
+                        exit = fadeOut()
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .background(MaterialTheme.colors.background)
+                                .wrapContentSize()
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+            for (i in 21..40) {
                 Text(
                     text = "Item $i",
                     modifier = Modifier
@@ -81,7 +179,7 @@ fun MapInColumn(
             Spacer(modifier = Modifier.padding(10.dp))
 
         }
-        
+
     }
 
 }
@@ -118,8 +216,8 @@ fun GoogleMapViewInColumn(
             title = "Singapore",
             onClick = markerClick,
             draggable = true
-            ){
-             
+        ) {
+
             Text(it.title ?: "Title", color = Color.Red)
         }
 
