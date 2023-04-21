10e3c4b549db37053361871a3b96cf3f05f2aefe
mehdi <dehghani_m@rocketmail.com>
2023-04-21 11:34:04 +0430

    continue malIncolumn

--- app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
+++ app/src/main/java/com/example/mapcompose/MapInColumnActivity.kt
@@ -2,25 +2,29 @@ package com.example.mapcompose
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.MarkerInfoWindowContent
 import com.google.maps.android.compose.rememberMarkerState
 
 private const val TAG = "ScrollingMapActivity"
 
-class MapInColumnActivity: ComponentActivity() {
+class MapInColumnActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +34,20 @@ class MapInColumnActivity: ComponentActivity() {
     }
 }
 
+
+@Composable
+fun MapInColumn(
+
+){
+    
+}
+
 @Composable
 fun GoogleMapViewInColumn(
     modifier: Modifier,
     cameraPositionState: CameraPositionState,
     onMapLoaded: () -> Unit
-){
+) {
     val singaporeState = rememberMarkerState(position = singapore)
 
     var uiSettings by remember { mutableStateOf(MapUiSettings(compassEnabled = false)) }
@@ -49,8 +61,23 @@ fun GoogleMapViewInColumn(
         properties = mapProperties,
         uiSettings = uiSettings,
         onMapLoaded = onMapLoaded
-    ){
-        val markerClick: (Marker)
+    ) {
+        val markerClick: (Marker) -> Boolean = {
+            Log.d(TAG, "${it.title} was clicked")
+            cameraPositionState.projection?.let { projection ->
+                Log.d(TAG, "The current projection is: $projection")
+            }
+            false
+        }
+        MarkerInfoWindowContent(
+            state = singaporeState,
+            title = "Singapore",
+            onClick = markerClick,
+            draggable = true
+            ){
+             
+            Text(it.title ?: "Title", color = Color.Red)
+        }
 
     }
 }
